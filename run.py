d272d4f435a3727063f3f9ca9ffe4f4b79d2d655
SamanthaBooth81 <samanthabooth09@live.co.uk>
2021-11-20 13:27:37 +0000

    Initial Commit

---
+++ run.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello, World"
+
+
+if __name__ == "__main__": 
+    # __main__ is the name of the default module in Python 
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
+# DO NOT PUT debug=True IN LIVE PROJECT, CHANGE TO debug=False for security reasons 
+# debug = True allows for arbitrary code to be run which is a security flaw
