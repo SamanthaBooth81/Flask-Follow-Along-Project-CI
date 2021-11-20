07e9541dd7928eee233fdfb364ba86d942a1a818
SamanthaBooth81 <samanthabooth09@live.co.uk>
2021-11-20 13:59:50 +0000

    Create contact page and renter to screen

--- run.py
+++ run.py
@@ -10,6 +10,16 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
 if __name__ == "__main__":
     # __main__ is the name of the default module in Python
     app.run(
