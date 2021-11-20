60ada2fa5206806c8c63309d3706c4a446eb95c9
SamanthaBooth81 <samanthabooth09@live.co.uk>
2021-11-20 13:28:36 +0000

    Tidy up code and fix problems

--- run.py
+++ run.py
@@ -10,11 +10,12 @@ def index():
     return "Hello, World"
 
 
-if __name__ == "__main__": 
-    # __main__ is the name of the default module in Python 
+if __name__ == "__main__":
+    # __main__ is the name of the default module in Python
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
         debug=True)
-# DO NOT PUT debug=True IN LIVE PROJECT, CHANGE TO debug=False for security reasons 
+# DO NOT PUT debug=True IN LIVE PROJECT, CHANGE TO
+# debug=False FOR SECURITY REASONS
 # debug = True allows for arbitrary code to be run which is a security flaw
